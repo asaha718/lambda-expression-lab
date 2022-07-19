f34ab794086811178ac23c9a0f706e44eab096fa
Anug Saha <anugsaha@gmail.com>
2022-07-19 13:04:42 -0400

    working lambda

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,15 @@ import java.util.function.IntUnaryOperator;
 
 public class Main {
 	// write the nextOddNum here
-    
+    @FunctionalInterface
+    interface OddNum{
+        int applyAsInt(int num);
+    }
     public static void main(String[] args) {
         int num = 5;
         int num2 = 10;
+
+        OddNum nextOddNum = (n) -> n % 2 == 0 ? n + 1 : n + 2;
 
         System.out.println(nextOddNum.applyAsInt(num)); // 7
         System.out.println(nextOddNum.applyAsInt(num2)); // 11
